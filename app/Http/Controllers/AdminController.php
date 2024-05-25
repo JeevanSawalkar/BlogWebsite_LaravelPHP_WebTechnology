a61c8eb22963cd51f3bdaa39b8bd83deca4b4838
JeevanSawalkar <sawalkarjeevan@gmail.com>
2024-05-25 19:54:13 -0400

    1st commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class AdminController extends Controller
+{
+    public function post_page()
+    {
+        return view('admin.post_page');
+    }
+    public function add_post(Request $request)
+    {
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->image = $request->image;
+        $post->save();
+
+
+        return view('admin.post_page');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[HomeController::class,'homepage']);
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+route::get('/home',[HomeController::class, 'index'])->middleware('auth')->name('home');
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->name('logout');
+
+// route::get('post',[HomeController::class,'post'])->middleware(['auth','admin'])->name('home');
+
+require __DIR__.'/auth.php';
+
+Route::get('/post_page',[AdminController::class,'post_page']);
+Route::post('/add_post',[AdminController::class,'add_post']);
